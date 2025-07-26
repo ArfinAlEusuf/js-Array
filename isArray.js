5d1d64799405ff6e93cf97f541222ee12b6e04b5
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-27 00:19:53 +0600

    isArray

---
+++ isArray.js
@@ -0,0 +1,11 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+const nums = [];
+
+const age = 15;
+const food = 'rosogo lla'
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age)); 
