d07d66d4194e22f54a8b3eae854c888c61077b6b
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-26 23:20:17 +0600

    araay add

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 // const numbers = 14216541;
 
 const numbers= [12, 22, 54, 65];
-const friends=['abul', 'babul', 'chabul']
+const friends=['abul', 'babul', 'chabul'];
+const passed= [true, false, true, true];
