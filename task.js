373cb4c7be6bbd97cb2001a9be0c23b241720453
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-27 00:31:37 +0600

    task done

---
+++ task.js
@@ -0,0 +1,14 @@
+// task 3
+// const fruits= ['apple', 'mango', 'pineApple', 'lichi', 'banana'];
+// console.log(fruits[3])
+
+// fruits[2]='jambura'
+// console.log(fruits);
+
+// task 2
+const place= ['bandarbon', 'syllet', 'cox bazar']
+place.push('durgapur');
+place.push('tangur hawor','panama');
+console.log(place);
+place.pop();
+
