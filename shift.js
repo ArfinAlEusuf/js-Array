b28df101f118efe8b678225faf359394aae863fc
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-26 23:55:18 +0600

    shift unshift

---
+++ shift.js
@@ -0,0 +1,9 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+console.log(friends);
+// remove item on first place
+friends.shift();
+console.log(friends);
+
+// add items on first place
+friends.unshift('ghumailam');
+console.log(friends)
