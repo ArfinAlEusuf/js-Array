8a2fc2010b1203230a3f11de1cdb7207bf3601af
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-27 00:01:50 +0600

    includes conditon

--- includes.js
+++ includes.js
@@ -1,3 +1,10 @@
 const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
 
-console.log(friends.includes('gelam'))
+console.log(friends.includes('gelam'));
+
+if(friends.includes('khailam')){
+    console.log('party')
+}
+else{
+    console.log('No food. we are fasting')
+}
