78db0fd6936911d611ff8012dbd05acc4d259f89
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-26 23:33:14 +0600

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [88, 77, 99, 58, 15, 55];
+
+console.log(numbers);
+
+// get elements by index
+console.log(numbers[2]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or uodate elements value by id
+let abc = 45;
+abc = 99;
+
+numbers[1]= 999;
+console.log(numbers);
